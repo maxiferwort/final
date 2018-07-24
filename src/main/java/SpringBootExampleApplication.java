fa8c5209bfa2771fd8b8b0136061236c7d7efa81
Maximiliano Jose Fernandez Wortman <maxifwortman@gmail.com>
2018-07-24 20:22:29 -0300

    final

--- src/main/java/SpringBootExampleApplication.java
+++ src/main/java/SpringBootExampleApplication.java
@@ -1,10 +1,18 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootExampleApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootExampleApplication.class, args);
+  }
+
+  @GetMapping("/hola")
+  public String hola() {
+    return "holaaas";
   }
 }
